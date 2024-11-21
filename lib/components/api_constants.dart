b7dde4c8af79a28f8c1f720a39c913071bce512b
GeekyCoder19 <110388756+GeekyCoder19@users.noreply.github.com>
2024-11-21 22:36:00 +0530

    Update api_constants.dart

--- lib/components/api_constants.dart
+++ lib/components/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String apiKey = 'AIzaSyBPk2i5hxX6QognnJw8kmEFNUUTFXS-NEw';
+  static const String apiKey = 'Random';
 
   static String baseUrl(String area) {
     return 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=doctor+in+$area&key=$apiKey';
